f37b1f00070b481a02681fa8571965eba0e2b37c
Lawrence Kelvin Nyarko <codercybr@gmail.com>
2023-09-16 10:29:06 -0700

    Handle c specifier

--- _printf.c
+++ _printf.c
@@ -9,5 +9,28 @@
 
 int _printf(const char *format, ...)
 {
-	
+	int count_char = 0;
+	char ch;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				ch = va_arg(args, int);
+				write(1, &ch, 1);
+				count_char++
+				break;
+			}
+		}
+		
+	}
+	return (count_char);
+
 }
+
