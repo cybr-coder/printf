f79eb997391b19feb6ae2ab858129799345a2621
“Ntowbea” <ntowbae@gmail.com>
2023-09-16 08:50:02 -0700

    Initial commit

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: format specifier
+ *
+ * Return: number of characters printed excluding the NULL byte
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}
