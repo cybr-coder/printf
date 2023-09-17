46996ab1e6a1522807513d2e534b9cd276d6970d
“Ntowbea” <ntowbae@gmail.com>
2023-09-17 13:12:41 -0700

    Initial commit

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
-			format++;
+			format++; //Proceed to the character immediately following '%'
 			if (*format == 'c')
 			{
 				ch = va_arg(args, int);
@@ -27,9 +27,27 @@ int _printf(const char *format, ...)
 				count_char++
 				break;
 			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char);
+				write(1, &str, 1);
+				str++;
+				count_char++;
+			}
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				count_char++;	
+			}	
 		}
-		
+		else 
+		{
+			write(1, format, 1); //print characters that are regular
+			count_char++;
+		}
+		format++;	
 	}
+	va_end(args);
 	return (count_char);
 
 }
