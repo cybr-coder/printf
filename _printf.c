6e4d6d4513f98256044a26e32a6e6918bbf36cd3
“Ntowbea” <ntowbae@gmail.com>
2023-09-19 13:56:03 -0700

    Handled the d and i specifiers

--- _printf.c
+++ _printf.c
@@ -138,3 +138,48 @@ void print_i(va_list args, int *count_char)
 		count_dig--;
 	}
 }
+/**
+ *_printf - Custom printf function
+ *
+ * Return: Number of characters printed (excluding null bytes)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count_char = 0;
+
+	va_start(args, format);
+
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == 'c')
+				print_c(args, &count_char);
+			else if (*format == 's')
+				print_s(args, &count_char);
+			else if (*format == 'd' || *format == 'i')
+				print_i(args, &count_char);
+			else if (*format == '%')
+				count_char += _putchar('%');
+			else
+			{
+				count_char += _putchar('%');
+				count_char += _putchar(*format);
+			}
+		}
+		else
+		{
+			count_char += _putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count_char);
+}
+
